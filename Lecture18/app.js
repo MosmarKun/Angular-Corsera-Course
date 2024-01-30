9d8e4c508323c463972a58c772871477e47068fc
Mosmar <hooosssen@gmail.com>
2024-01-30 18:58:40 +0300

    lecture 18 modifed

--- Lecture18/app.js
+++ Lecture18/app.js
@@ -4,13 +4,27 @@
 var shoppingList = [
   "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
 ];
+var childshoppingList = [
+  "Milk", "Donuts", "Cookies", "Pepto Bismol (Cookie flavor)"
+];
 
 angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+.controller('ShoppingListController', ShoppingListController)
+.controller('ChildShoppingListController', ChildShoppingListController);
 
 ShoppingListController.$inject = ['$scope'];
 function ShoppingListController($scope) {
   $scope.shoppingList = shoppingList;
+  $scope.pc = this;
+  $scope.pc.shoppingList = shoppingList;
+
+}
+ChildShoppingListController.$inject = ['$scope'];
+function ChildShoppingListController($scope) {
+  $scope.shoppingList = childshoppingList;
+  console.log($scope);
+  console.log($scope.pc.shoppingList);
+
 }
 
 })();
